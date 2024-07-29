aaf3040ddf6d04b0e8a905a7000216d0ef91b993
TatyanaPolet <tatyana_polet@mail.ru>
2024-07-29 11:53:30 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide(first,second):
+    if second == 0:
+        return "Ошибка"
+    else:
+        return first / second
+
+# print(divide(49, 7))
+# print(divide(15, 0))
---
+++ main.py
@@ -0,0 +1,34 @@
+
+import fake_math as fm
+import true_math as tm
+
+
+
+fake_divide = fm.divide
+true_divide = tm.divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
